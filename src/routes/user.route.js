0cf07a45eb6a0657958e5e4c13f041e34845ec2c
alok <alokrathore136@gmail.com>
2025-09-05 13:30:31 +0530

    made getUser Put

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -8,6 +8,6 @@ router.route('/register').post(registerUser);
 router.route('/login').post(loginUser);
 router.route('/updateUser').put(upload.single('avatar'),updateUserProfile);
 router.route('/getAllUsers').get(getAllUsers)
-router.route('/getUser').get(getUser)
+router.route('/getUser').put(getUser)
 
 export default router
